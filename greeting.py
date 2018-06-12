acc9943fb01471637abffb0d96d1c7f8ce020abe
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-06-12 01:07:00 -0700

    Commit Update

---
+++ greeting.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+a = Flask(__name__)
+
+@a.route("/")
+def greeting():
+    return "Hello, I am May Thu Htun"
+
+@a.route("/tiide")
+def tiide():
+    return "I am from Computer University Mandalay"
